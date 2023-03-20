51e90c5fd51fa2f3732b1738368cf0fe14123c3c
Nasir Hafeez <nasirhafeez2002@gmail.com>
2023-03-20 10:53:34 +0500

    podio test

---
+++ default/podio.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'header.php';
+
+$client_id = $_SERVER['CLIENT_ID'];
+$client_secret = $_SERVER['CLIENT_SECRET'];
+$app_id = $_SERVER['APP_ID'];
+$app_token = $_SERVER['APP_TOKEN'];
+
+Podio::setup($client_id, $client_secret);
+Podio::authenticate_with_app($app_id, $app_token);
+$items = PodioItem::filter($app_id);
+
+print "My app has " . count($items) . " items";
