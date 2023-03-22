2168a22d2bb142de40102c61f5d71ac588d64bd7
Nasir Hafeez <nasirhafeez2002@gmail.com>
2023-03-22 18:25:12 +0500

    podio test

--- default/podio.php
+++ default/podio.php
@@ -11,13 +11,9 @@ Podio::setup($client_id, $client_secret);
 Podio::authenticate_with_app($app_id, $app_token);
 $items = PodioItem::filter($app_id);
 
-print "The collection contains ".count($items)." items";
-print "There are ".$items->total." items in the app in total";
-print "There are ".$items->filtered." items with the current filter applied";
-
-//$item = new PodioItem();
+$item = new PodioItem();
 //$item->fields['app-reference']->values = array('user' => 250138701);
 //$item->fields['app-reference']->values = array('location' => 250138701);
-//$item->fields['date']->start = "2011-12-31 11:27:10";
-//$item->fields['text']->values = '12:e5:10:47:b9:72';
-//$item->save();
+$item->fields['date']->start = "2011-12-31 11:27:10";
+$item->fields['text']->values = '12:e5:10:47:b9:72';
+$item->save();
