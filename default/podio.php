4a37653d99f38c15afeca8450099453072fe94cb
Nasir Hafeez <nasirhafeez2002@gmail.com>
2023-03-22 16:28:54 +0500

    podio test

--- default/podio.php
+++ default/podio.php
@@ -14,7 +14,9 @@ $items = PodioItem::filter($app_id);
 foreach ($items as $item) {
   foreach ($item->fields as $field) {
     // You can now work on each individual field object:
-    print "This field has the id: ".$field->field_id . '\n';
+    print "This field has the id: ".$field->field_id;
+    echo "<br>";
     print "This field has the external_id: ".$field->external_id;
+    echo "<br>";
   }
 }
