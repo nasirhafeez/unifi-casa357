d97fad50014483a180085ada9e16b44c1a236e82
Nasir Hafeez <nasirhafeez2002@gmail.com>
2023-03-23 11:24:45 +0500

    podio test

--- default/podio_user.php
+++ default/podio_user.php
@@ -12,19 +12,21 @@ $app_token = $app_token_user;
 Podio::setup($client_id, $client_secret);
 Podio::authenticate_with_app($app_id, $app_token);
 
-$fields = new PodioItemFieldCollection(array(
-  new PodioTextItemField(array("external_id" => "first-name", "values" => "John")),
-  new PodioTextItemField(array("external_id" => "family-name", "values" => "Doe")),
-  new PodioTextItemField(array("external_id" => "mobile-phone-number", "values" => "123456789")),
-  new PodioEmailItemField(array("external_id" => "email", "values" => array(
-    'type' => "work",
-    'value' => "abc@xyz.com"
-  )))
-));
+print gettype($app_id);
 
-$item = new PodioItem(array(
-  'app' => new PodioApp($app_id),
-  'fields' => $fields
-));
-
-$item->save();
+//$fields = new PodioItemFieldCollection(array(
+//  new PodioTextItemField(array("external_id" => "first-name", "values" => "John")),
+//  new PodioTextItemField(array("external_id" => "family-name", "values" => "Doe")),
+//  new PodioTextItemField(array("external_id" => "mobile-phone-number", "values" => "123456789")),
+//  new PodioEmailItemField(array("external_id" => "email", "values" => array(
+//    'type' => "work",
+//    'value' => "abc@xyz.com"
+//  )))
+//));
+//
+//$item = new PodioItem(array(
+//  'app' => new PodioApp($app_id),
+//  'fields' => $fields
+//));
+//
+//$item->save();
