059808b3f4c1dd48650ce7694fca1eb614f9dddf
Nasir Hafeez <nasirhafeez2002@gmail.com>
2023-03-23 11:07:36 +0500

    podio test

--- default/podio_session.php
+++ default/podio_session.php
@@ -19,40 +19,43 @@ Podio::setup($client_id, $client_secret);
 Podio::authenticate_with_app($app_id, $app_token);
 $items = PodioItem::filter($app_id);
 
-foreach ($items as $item) {
-  print $item->item_id;
-  echo "<br>";
-  foreach ($item->fields as $field) {
-    print "This field has the external_id: ".$field->external_id;
-    echo "<br>";
-    $collection = $field->values;
-    echo gettype($collection) . "<br>";
-    if (gettype($collection) == "object")
-    foreach ($collection as $referenced_item) {
-      print "Referenced item: ".$referenced_item->title;
-      echo "<br>";
-    } elseif (gettype($collection) == "array") {
-      print $field->humanized_value();
-      echo "<br>";
-    } else {
-      print $collection . "<br>";
-    }
-  }
-}
+//foreach ($items as $item) {
+//  print $item->item_id;
+//  echo "<br>";
+//  foreach ($item->fields as $field) {
+//    print "This field has the external_id: ".$field->external_id;
+//    echo "<br>";
+//    $collection = $field->values;
+//    echo gettype($collection) . "<br>";
+//    if (gettype($collection) == "object")
+//    foreach ($collection as $referenced_item) {
+//      print "Referenced item: ".$referenced_item->title;
+//      echo "<br>";
+//    } elseif (gettype($collection) == "array") {
+//      print $field->humanized_value();
+//      echo "<br>";
+//    } else {
+//      print $collection . "<br>";
+//    }
+//  }
+//}
 
-//$fields = new PodioItemFieldCollection(array(
-//  new PodioTextItemField(array("external_id" => "first-name", "values" => "John")),
-//  new PodioTextItemField(array("external_id" => "family-name", "values" => "Doe")),
-//  new PodioTextItemField(array("external_id" => "mobile-phone-number", "values" => "123456789")),
-//  new PodioEmailItemField(array("external_id" => "email", "values" => array(
-//    'type' => "home",
-//    'value' => "abc@xyz.com"
-//  )))
-//));
-//
-//$item = new PodioItem(array(
-//  'app' => new PodioApp($app_id),
-//  'fields' => $fields
-//));
-//
-//$item->save();
+$fields = new PodioItemFieldCollection(array(
+  new PodioAppItemField(array("external_id" => "user", "values" => array(
+    'item_id' => "2416065557"
+  ))),
+  new PodioAppItemField(array("external_id" => "location", "values" => array(
+    'item_id' => "Casa357"
+  ))),
+  new PodioDateItemField(array("external_id" => "login-time", "values" => array(
+    "start" => "2023-03-19 06:29"
+  ))),
+  new PodioTextItemField(array("external_id" => "mac-address", "values" => "12:e5:10:47:b9:72"))
+));
+
+$item = new PodioItem(array(
+  'app' => new PodioApp($app_id),
+  'fields' => $fields
+));
+
+$item->save();
--- default/podio_user.php
+++ default/podio_user.php
@@ -14,16 +14,13 @@ Podio::authenticate_with_app($app_id, $app_token);
 $items = PodioItem::filter($app_id);
 
 $fields = new PodioItemFieldCollection(array(
-  new PodioAppItemField(array("external_id" => "user", "values" => array(
-    'item_id' => "2416065557"
-  ))),
-  new PodioAppItemField(array("external_id" => "location", "values" => array(
-    'item_id' => "Casa357"
-  ))),
-  new PodioDateItemField(array("external_id" => "login-time", "values" => array(
-    "start" => "2023-03-19 06:29"
-  ))),
-  new PodioTextItemField(array("external_id" => "mac-address", "values" => "12:e5:10:47:b9:72"))
+  new PodioTextItemField(array("external_id" => "first-name", "values" => "John")),
+  new PodioTextItemField(array("external_id" => "family-name", "values" => "Doe")),
+  new PodioTextItemField(array("external_id" => "mobile-phone-number", "values" => "123456789")),
+  new PodioEmailItemField(array("external_id" => "email", "values" => array(
+    'type' => "home",
+    'value' => "abc@xyz.com"
+  )))
 ));
 
 $item = new PodioItem(array(
