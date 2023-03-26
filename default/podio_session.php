14059129ac352475e870f8edaeb47f762537eef1
Nasir Hafeez <nasirhafeez2002@gmail.com>
2023-03-26 12:22:56 +0500

    podio test

--- default/podio_session.php
+++ default/podio_session.php
@@ -19,14 +19,8 @@ $items = PodioItem::filter($app_id);
 foreach ($items as $item) {
   foreach ($item->fields as $field) {
     if ($field->external_id == "location") {
-      print $field->field_id;
-      $collection = $field->values;
-      echo gettype($collection) . "<br>";
-      if (gettype($collection) == "object")
-        foreach ($collection as $referenced_item) {
-          print "Referenced item: ".$referenced_item->title;
-          echo "<br>";
-        }
+      $collection = $item->fields['app-reference']->values;
+      var_dump($collection);
     }
   }
 }
