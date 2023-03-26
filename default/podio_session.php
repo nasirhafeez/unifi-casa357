b858f2f368d6ea67fcd33b4563c67ab181843b1f
Nasir Hafeez <nasirhafeez2002@gmail.com>
2023-03-26 13:18:54 +0500

    podio test

--- default/podio_session.php
+++ default/podio_session.php
@@ -31,7 +31,7 @@ Podio::authenticate_with_app($app_id, $app_token);
 //  }
 //}
 
-$item = PodioItem::get_basic(2422645833);
+$item = PodioItem::get(2422645833);
 //$item->fields[250138701]->values = new PodioItem(array('item_id' => 2422645835));
 
 $item = new PodioItem(array('fields' =>
@@ -41,4 +41,5 @@ $item = new PodioItem(array('fields' =>
     )))
   ))
 ));
+
 $item->save();
